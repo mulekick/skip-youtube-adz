9c9296617b5791c7055b2e69d440f4def32b5dc5
mulekick <karim_laouari@hotmail.com>
2022-05-30 17:18:39 +0200

    autoclose popup ads

--- chrome/youtube-skip-ads.js
+++ chrome/youtube-skip-ads.js
@@ -7,11 +7,17 @@ const
     // ad manager
     YT_AD_MANAGER_SELECTOR = `#movie_player > div.video-ads.ytp-ad-module`,
     YT_AD_MANAGER_DIV_STYLE = `video-ads ytp-ad-module`,
-    // ad overlay
-    YT_AD_OVERLAY_SELECTOR = `#movie_player > div.video-ads.ytp-ad-module > div.ytp-ad-player-overlay`,
-    YT_AD_OVERLAY_DIV_STYLE = `ytp-ad-player-overlay`,
-    // skippable ad
-    YT_AD_SKIPPABLE_DIV_STYLE = `ytp-ad-player-overlay-skip-or-preview`,
+    // video ad overlay
+    YT_VIDEO_AD_OVERLAY_SELECTOR = `#movie_player > div.video-ads.ytp-ad-module > div.ytp-ad-player-overlay`,
+    YT_VIDEO_AD_OVERLAY_STYLE = `ytp-ad-player-overlay`,
+    // skippable video ad
+    YT_VIDEO_AD_SKIPPABLE_STYLE = `ytp-ad-player-overlay-skip-or-preview`,
+    // popup ad overlay
+    YT_POPUP_AD_OVERLAY_STYLE = `ytp-ad-overlay-slot`,
+    // closeable popup ad
+    YT_POPUP_AD_CLOSEABLE_STYLE = `ytp-ad-overlay-close-button`,
+    // debug flag
+    PRINT_DEBUG_MSG = false,
     // create node subtree observer
     createNodeSubtreeObserver = (classMatch, idMatch, tagMatch, executeOnAppend, executeOnRemove, disconnect, resolve, reject) => {
         try {
@@ -28,7 +34,8 @@ const
                                 // if it is a match
                                 if (addedNode.nodeType === ELEMENT_NODE && addedNode.tagName === tagMatch && (addedNode.id === idMatch || String(addedNode.classList) === classMatch)) {
                                     // log
-                                    // console.log(`${ EXTENSION_NAME }: ${ addedNode.tagName } element with id ${ addedNode.id } and classes ${ addedNode.classList } added to DOM.`);
+                                    if (PRINT_DEBUG_MSG === true)
+                                        console.log(`${ EXTENSION_NAME }: ${ addedNode.tagName } element with id ${ addedNode.id } and classes ${ addedNode.classList } added to DOM.`);
                                     // execute
                                     if (executeOnAppend && typeof executeOnAppend === `function`)
                                         executeOnAppend(addedNode);
@@ -46,7 +53,8 @@ const
                                 // if it is a match
                                 if (removedNode.nodeType === ELEMENT_NODE && removedNode.tagName === tagMatch && (removedNode.id === idMatch || String(removedNode.classList) === classMatch)) {
                                     // log
-                                    // console.log(`${ EXTENSION_NAME }: ${ removedNode.tagName } element with id ${ removedNode.id } and classes ${ removedNode.classList } removed from DOM.`);
+                                    if (PRINT_DEBUG_MSG === true)
+                                        console.log(`${ EXTENSION_NAME }: ${ removedNode.tagName } element with id ${ removedNode.id } and classes ${ removedNode.classList } removed from DOM.`);
                                     // execute
                                     if (executeOnRemove && typeof executeOnRemove === `function`)
                                         executeOnRemove(removedNode);
@@ -93,25 +101,51 @@ const
                 reject(err);
             }
         }),
-    // skip ad from overlay
+    // skip video ad
     skip = overlay => {
         try {
             // log
-            console.log(`${ EXTENSION_NAME }: ad is beginning`);
+            console.log(`${ EXTENSION_NAME }: ad is beginning.`);
             // retrieve ad type
             const ad = overlay.childNodes.item(2);
             // if ad is skippable
-            if (ad instanceof Element && String(ad.classList) === YT_AD_SKIPPABLE_DIV_STYLE) {
+            if (ad instanceof Element && String(ad.classList) === YT_VIDEO_AD_SKIPPABLE_STYLE) {
                 // retrieve skip button
                 ad.firstChild.childNodes.item(1).firstChild.firstChild
                     // fire click event
                     .click();
                 // log
                 console.log(`${ EXTENSION_NAME }: ad skipped.`);
-            }
+            // if not
+            } else
+                // throw a generic error
+                throw new Error();
         } catch (err) {
             // log
             console.log(`${ EXTENSION_NAME }: ad is not skippable.`);
+        }
+    },
+    // close overlayed ad
+    close = overlay => {
+        try {
+            // log
+            console.log(`${ EXTENSION_NAME }: ad is showing.`);
+            // retrieve close button
+            const cl = overlay.firstChild.firstChild.childNodes.item(1).firstChild;
+            // if ad is closeable
+            if (cl instanceof Element && String(cl.classList) === YT_POPUP_AD_CLOSEABLE_STYLE) {
+                cl
+                    // fire click event
+                    .click();
+                // log
+                console.log(`${ EXTENSION_NAME }: ad closed.`);
+            // if not
+            } else
+                // throw a generic error
+                throw new Error();
+        } catch (err) {
+            // log
+            console.log(`${ EXTENSION_NAME }: could not close ad.`);
         }
     };
 
@@ -126,23 +160,28 @@ const
             [ observer, observed ] = [ null, null ];
 
         // in the event a YT url is directly pasted in the browser's bar, the ad overlay is instantly loaded
-        observed = document.querySelector(YT_AD_OVERLAY_SELECTOR);
+        // let's thus try to maximize the chances of getting a monitorable handle for the ad management div
+        // by traversing the hierarchy of selectors to it and testing if the corresponding elements are loaded
+        // when the script executes
+
+        // attempt to retrieve video ad overlay
+        observed = document.querySelector(YT_VIDEO_AD_OVERLAY_SELECTOR);
 
         // if no ad is playing when the DOM is done loading
         if (observed === null) {
 
-            // let'ssss go ...
             console.log(`${ EXTENSION_NAME }: waiting for movie player div to load.`);
 
+            // attempt to retrieve video player
             observed = document.querySelector(`#movie_player`);
 
             if (observed === null)
                 // wait for video player to load
                 observed = await retrieveChildNode(`body`, `movie_player`, null, `DIV`);
 
-            // log
             console.log(`${ EXTENSION_NAME }: movie player div loaded, waiting for ad management layer.`);
 
+            // attempt to retrieve ad management layer
             observed = document.querySelector(YT_AD_MANAGER_SELECTOR);
 
             if (observed === null)
@@ -152,7 +191,6 @@ const
         // else
         } else {
 
-            // let'ssss go ...
             console.log(`${ EXTENSION_NAME }: ad overlay already loaded, attempting skip.`);
 
             // skip the initial ad
@@ -162,11 +200,43 @@ const
             observed = document.querySelector(YT_AD_MANAGER_SELECTOR);
         }
 
-        // log
-        console.log(`${ EXTENSION_NAME }: ad management layer loaded, monitoring skippable ads.`);
+        console.log(`${ EXTENSION_NAME }: ad management layer loaded, monitoring video and popup ads.`);
 
         // setup skip button observer
-        observer = createNodeSubtreeObserver(YT_AD_OVERLAY_DIV_STYLE, null, `DIV`, skip, null, false, null, null);
+        observer = createNodeSubtreeObserver(
+            YT_VIDEO_AD_OVERLAY_STYLE,
+            null,
+            `DIV`,
+            // video ad begins
+            skip,
+            // video ad ends
+            null,
+            // keep observer running
+            false,
+            null,
+            null);
+
+        // start observing
+        observer.observe(observed, {
+            // observe entire node subtree
+            subtree: true,
+            // for addition/removal of child nodes
+            childList: true
+        });
+
+        // setup popup ads observer
+        observer = createNodeSubtreeObserver(
+            YT_POPUP_AD_OVERLAY_STYLE,
+            null,
+            `DIV`,
+            // popup ad begins
+            close,
+            // popup ad ends
+            null,
+            // keep observer running
+            false,
+            null,
+            null);
 
         // start observing
         observer.observe(observed, {
